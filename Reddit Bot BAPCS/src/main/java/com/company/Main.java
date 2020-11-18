071b4cd38b1b878673bcbbeba9bb6ab467007de8
Dwight McLeish Jr <dmcleishjr@gmail.com>
2020-11-18 00:50:25 -0500

    Add files via upload

--- Reddit Bot BAPCS/src/main/java/com/company/Main.java
+++ Reddit Bot BAPCS/src/main/java/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 import Application.RedditInterface;
 
 /**
- * The HelloWorld program implements an application that
- * simply displays "Hello World!" to the standard output.
+ * This software application runs a script that parces a publicly maintained
+ * csv file of ssd information and post that information in a formatted mannar
+ * on the subreddit Buildapcsales
  *
  * @author  Dwight McLeish Jr
  * @version 0.1
